c55af03c47c7fe4692367d77dd521098de90dae2
Nawaz Danish <nawazdanish1996@gmail.com>
2022-12-06 12:44:15 +0530

    updated

--- src/components/Home.js
+++ src/components/Home.js
@@ -24,23 +24,24 @@ function Home() {
               <img loading="lazy" id='profileImg' src={myPic} alt="myPic" />
             </div>
             <div style={{textAlign: "justify"}} className="col-md-6 mt-md-2 mt-sm-2">
-              <h1 className="mt-md-5 mt-sm-5">Hi! I'm <span>Nawaz Danish</span></h1>
-              <h3 className="mt-md-2 mt-sm-2 text-success">
+              <h3 className="mt-md-5 mt-sm-5">Hi! I'm <span>Nawaz Danish</span></h3>
+              <h2 className="mt-md-2 mt-sm-2 text-success">
                 {text}
                 <Cursor cursorColor="red" />
-              </h3>
+              </h2>
               <p className="mt-md-2 mt-sm-2">I am a React Developer with hands-on experience in identifying web-based user interactions along with highly responsive user interface components by deploying react concepts and writing code via Javascript and React.js workflows. Monitoring and maintaining frontend performance.</p>
             </div>
           </div>
 
           <div class="row mt-md-5 mt-5">
             <div className="col-md-6 mb-3">
-              <a href="https://github.com/nawazdanish1996" target="blank"><i class="fa-brands fa-square-github"></i></a>
-              <a href="https://www.linkedin.com/in/nawazdanish/" target="blank"><i class="fa-brands fa-linkedin"></i></a>
-              <a href="https://twitter.com/nawazdanish1996" target="blank"><i class="fa-brands fa-square-twitter"></i></a>
+              <a href="https://github.com/nawazdanish1996" target="blank"><i className="fa-brands fa-square-github"></i></a>
+              <a href="https://www.linkedin.com/in/nawazdanish/" target="blank"><i className="fa-brands fa-linkedin"></i></a>
+              <a href="https://twitter.com/nawazdanish1996" target="blank"><i className="fa-brands fa-square-twitter"></i></a>
+              <a href="https://www.facebook.com/developerHost/" target="blank"><i className="fa-brands fa-square-facebook"></i></a>
             </div>
             <div className="col-md-6 mb-2">
-              <a id="resume" href={Resume} target="blank">Download Resume<i class="fa-solid fa-download"></i></a>
+              <a id="resume" href={Resume} target="blank">Get Resume<i class="fa-solid fa-download"></i></a>
             </div>
           </div>
       </div>
